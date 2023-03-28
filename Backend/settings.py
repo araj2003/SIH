b051375c32077c174a857d1b07763d1d0fd1efd1
Kavya Dua <114342842+K-1303@users.noreply.github.com>
2023-03-29 00:29:38 +0530

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'K@13032003',
+        'USER': '[Enter yout postgresql username]',
+        'PASSWORD': '[Enter Your postgresql password]',
         'HOST': 'localhost',
         'PORT': 5432,
     }
