8a8ea2f665aa55ea2272eacde13a9f0db7246935
Kavya Dua <kavyadua03@gmail.com>
2023-03-28 23:13:34 +0530

    "saving changes"

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,36 +1,35 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+class AppUserManager(BaseUserManager):
+	def create_user(self, email, password=None):
+		if not email:
+			raise ValueError('An email is required.')
+		if not password:
+			raise ValueError('A password is required.')
+		email = self.normalize_email(email)
+		user = self.model(email=email)
+		user.set_password(password)
+		user.save()
+		return user
+	def create_superuser(self, email, password=None):
+		if not email:
+			raise ValueError('An email is required.')
+		if not password:
+			raise ValueError('A password is required.')
+		user = self.create_user(email, password)
+		user.is_superuser = True
+		user.save()
+		return user
 
-class UserAccountManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
-        if not email:
-            raise ValueError('Users must have an email address')
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
-
-        user.set_passsword(password)
-        user.save()
-
-        return user
-
-class UserAccount(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-
-    objects = UserAccountManager()
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
-
-    def get_full_name(self):
-        return self.name
-
-    def get_short_name(self):
-        return self.name
-    
-    def __str__(self):
-        return self.email
+class AppUser(AbstractBaseUser, PermissionsMixin):
+	user_id = models.AutoField(primary_key=True)
+	email = models.EmailField(max_length=50, unique=True)
+	username = models.CharField(max_length=50)
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
+	objects = AppUserManager()
+	def __str__(self):
+		return self.username
--- Backend/settings.py
+++ Backend/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Application definition
 
@@ -38,8 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
+    'corsheaders',
     'Accounts',
+    
     
 ]
 
@@ -90,11 +98,16 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'adarshdevloper167@gmail.com'
-EMAIL_USE_TLS = True
+AUTH_USER_MODEL = 'Accounts.AppUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
 
 
 # Password validation
@@ -140,31 +153,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        (...)
-    ),
-}
-
-SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
-}
-
-
-DJOSER = {
-    'LOGIN_FIELD': 'email',
-    'USER_CREATE_PASSWORD_RETYPE': True,
-    'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
-    'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
-    'SEND_CONFIRMATION_EMAIL': True,
-    'SET_USERNAME_RETYPE': True,
-    'SET_PASSWORD_RETYPE': True,
-    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-    'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': 'activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True,
-}
-
-AUTH_USER_MODEL = 'Accounts.UserAccount'
