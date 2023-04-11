84c54640a0d4cc890d7b5a89c486b2d28286d5b5
Kavya Dua <kavyadua03@gmail.com>
2023-04-11 22:50:09 +0530

    "saving changes"

--- DiseasePredictor/views.py
+++ DiseasePredictor/views.py
@@ -6,7 +6,7 @@ from django_pandas.io import read_frame
 from sklearn import preprocessing
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.preprocessing import StandardScaler
-from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 def scale_dataset(dataframe, oversample=False):
   X = dataframe[dataframe.columns[:-1]].values
@@ -27,25 +27,32 @@ def scale_dataset(dataframe, oversample=False):
 def model(request):
     data = read_frame(patient_data.objects.all())
     unique1 = data['prognosis'].unique()
-    label_encoder = preprocessing.LabelEncoder()
-    data['prognosis'] = label_encoder.fit_transform(data['prognosis'])
-    unique2 = data['prognosis'].unique()
 
     data_dict = data.to_dict('records')
     context = {'data': data_dict}
 
     train, X, Y = scale_dataset(data, oversample=True)
 
-    nb_model = GaussianNB()
-    nb_model = nb_model.fit(X, Y)
+    svm_model = SVC(probability=True)
+    svm_model = svm_model.fit(X, Y)
     x = np.zeros(130)
+    x[1] = 1
+    x[0] = 1
     x_ = x.reshape(1, -1)
-    Y_ = nb_model.predict(x_)
-    indices = np.where(unique2 == Y_)[0]
-    
+    Y_ = svm_model.predict(x_)
+
+    probas = svm_model.predict_proba(x_)
+
+    top5_indices = np.argsort(probas, axis=1)[:, -5:]
+
+    # Get the corresponding class labels
+    top5_labels = svm_model.classes_[top5_indices]
+
+    # Print the top 5 class labels for the first sample in the test data
+    print(top5_labels[0][::-1])
+
     print(unique1)
-    print(unique2)
-    print(unique1[indices[0]])
+    print(probas)
     return render(request, 'data.html', context)
 
     
