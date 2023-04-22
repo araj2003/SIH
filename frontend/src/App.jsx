4bab97734be44ae038d9e12542e9236e7f9020ee
Tanpreet Jolly <tanpreet2002@gmail.com>
2023-04-22 22:13:25 +0530

    added a main component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
 import "./App.css";
 import React from "react";
 import Header from "./assets/components/Header";
-import Modal from "./assets/components/Modal";
-import { useGlobalContext } from "./assets/components/context";
+import Main from "./assets/components/Main";
 
 function App() {
-  const { currentUser } = useGlobalContext();
-
   return (
     <div>
       <Header />
-      {!currentUser && <Modal />}
+      <Main />
     </div>
   );
 }
