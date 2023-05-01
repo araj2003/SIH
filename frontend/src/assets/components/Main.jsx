ac4591d1eb6266d7d9332bc6881ea7520a1446c4
Tanpreet Jolly <tanpreet2002@gmail.com>
2023-05-01 23:04:22 +0530

    render after login

--- frontend/src/assets/components/Main.jsx
+++ frontend/src/assets/components/Main.jsx
@@ -4,15 +4,24 @@ import Notice from "./Notice";
 import Hero from "./Hero";
 import Services from "./Services";
 import About from "./About";
+import PatientForm from "./PatientForm";
+
 const Main = () => {
   const { currentUser } = useGlobalContext();
+  if (!currentUser) {
+    return (
+      <main className="flex items-center flex-col min-h-screen">
+        <Hero />
+        <Services />
+        <About />
+        <Modal />
+      </main>
+    );
+  }
   return (
     <main className="flex items-center flex-col min-h-screen">
-      <Hero />
-      <Services />
-      <About />
       <Notice />
-      {!currentUser && <Modal />}
+      <PatientForm />
     </main>
   );
 };
