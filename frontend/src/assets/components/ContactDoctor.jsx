4103e2be8c72bd721b648e1a7c4831ca0d68402a
Kavya Dua <kavyadua03@gmail.com>
2023-05-26 23:45:12 +0530

    Setup Contact Doctor

--- frontend/src/assets/components/ContactDoctor.jsx
+++ frontend/src/assets/components/ContactDoctor.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ContactDoctor = () => {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/doctor/Dentist');
+        // Set the doctors data in state
+        setDoctors(response.data);
+      } catch (error) {
+        // Handle error here
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      Contact Doctor
+      {doctors.map((doctor) => (
+        <div key={doctor.id}>
+          <h3>{doctor.name}</h3>
+          <p>Speciality: {doctor.speciality}</p>
+          <p>Experience: {doctor.experience} years</p>
+          <p>Work Address: {doctor.work_address}</p>
+          <p>Mobile Number: {doctor.mobile_no}</p>
+          <img src={doctor.image_link} alt="Doctor" />
+          <a href={doctor.profile_link}>View Profile</a>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ContactDoctor
+export default ContactDoctor;
