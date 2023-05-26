c93414c6e1c79f34e90f41cad4dffdc4d5265fa3
Tanpreet Jolly <tanpreet2002@gmail.com>
2023-05-26 17:42:46 +0530

    Disease Predictor Functionality Completed

--- frontend/src/assets/components/Prediction.jsx
+++ frontend/src/assets/components/Prediction.jsx
@@ -14,13 +14,13 @@ const Prediction = ({ prediction }) => {
       <div className="bg-teal-50 h-full mt-1 flex flex-col justify-evenly rounded-lg p-1 ">
         {prediction.length > 0 &&
           prediction[0].diseases.map((disease, index) => (
-            <p
+            <div
               key={index}
               className="rounded-md m-1 py-1 px-2 bg-sky-100 text-lg text-indigo-950 flex justify-between"
             >
               <div>{disease}</div>
               <div>{handleProbability(prediction[0].diseases_prob[index])}</div>
-            </p>
+            </div>
           ))}
       </div>
       <div className=" p-2 bg-violet-100 mt-1 rounded-md uppercase text-center">
--- frontend/src/assets/components/searchSymptoms.jsx
+++ frontend/src/assets/components/searchSymptoms.jsx
@@ -1,33 +1,42 @@
 import React from "react";
-import { Autocomplete } from "@mui/material";
+import { Autocomplete, Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import { useGlobalContext } from "./context";
 
 const SymptomSearch = ({
   selectedSymptom,
   setSelectedSymptom,
-  handleKeyDown,
+  handleAddSymptom,
 }) => {
   const { options } = useGlobalContext();
 
   return (
-    <Autocomplete
-      options={options}
-      value={selectedSymptom}
-      onChange={(e, newValue) => {
-        setSelectedSymptom(newValue);
-      }}
-      className="w-1/3"
-      renderInput={(params) => (
-        <TextField
-          variant="outlined"
-          color="primary"
-          {...params}
-          label="Enter your symptoms.."
-          onKeyDown={handleKeyDown}
-        />
-      )}
-    />
+    <div className="flex w-3/5 justify-center gap-5 items-center">
+      <Autocomplete
+        options={options}
+        value={selectedSymptom}
+        onChange={(e, newValue) => {
+          setSelectedSymptom(newValue);
+        }}
+        className="w-1/2 bg-white"
+        renderInput={(params) => (
+          <TextField
+            variant="outlined"
+            color="primary"
+            {...params}
+            label="Enter your symptoms.."
+          />
+        )}
+      />
+      <Button
+        variant="outlined"
+        color="info"
+        onClick={handleAddSymptom}
+        size="large"
+      >
+        Add
+      </Button>
+    </div>
   );
 };
 
