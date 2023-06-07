95dbb86779f6fa8ad60a0ffab870780cb39779a7
Kavya Dua <kavyadua03@gmail.com>
2023-06-08 01:14:41 +0530

    added password validation

--- frontend/src/assets/components/RegisterForm.jsx
+++ frontend/src/assets/components/RegisterForm.jsx
@@ -12,6 +12,23 @@ const RegisterForm = () => {
   const [user_email, setUserEmail] = useState();
   const [user_username, setUserUsername] = useState();
   const [user_password, setUserPassword] = useState();
+
+  // Regular expression for password check
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+  const isPasswordValid = (password) => {
+    return passwordRegex.test(password);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevents the default form submission
+    if (isPasswordValid(user_password)) {
+      submitRegistration(event); // Call the submitRegistration function if the password is valid
+    } else {
+      console.log("Invalid password"); // Optional: Display an error message or perform other actions for an invalid password
+    }
+  };
+
   return (
     <div>
       <div className="flex justify-end mb-2 mr-2 ">
@@ -20,7 +37,7 @@ const RegisterForm = () => {
         </button>
       </div>
       <div className="flex flex-col gap-2">
-        <div className="flex flex-col  items-center gap-2 ">
+        <div className="flex flex-col items-center gap-2">
           <h2 className="text-4xl modal-heading text-center full">
             Sign Up Now!
           </h2>
@@ -28,7 +45,7 @@ const RegisterForm = () => {
             Access personalized healthcare services
           </p>
         </div>
-        <form onSubmit={submitRegistration} className="flex flex-col gap-2 j">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 j">
           <TextField
             id="FormBasicEmail"
             label="Email"
@@ -63,10 +80,15 @@ const RegisterForm = () => {
               setUserPassword(event.target.value);
               password.current = event.target.value;
             }}
-            color="success"
+            color={isPasswordValid(user_password) ? "success" : "error"}
             type="password"
             required
           />
+          {!isPasswordValid(user_password) && (
+            <p className="text-red-500 text-sm" style={{ fontSize: '9px', color: 'red' }}>
+            Password must be at least 8 characters long, contain at least 1 letter, 1 digit, and 1 special character.
+            </p>
+          )}
           <Button variant="outlined" color="primary" type="submit">
             Submit
           </Button>
