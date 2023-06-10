ea0e0a8203188afa0cc10e08de696cc8ac8688e0
Tanpreet Jolly <tanpreet2002@gmail.com>
2023-06-10 17:38:45 +0530

    Final Fixes

--- frontend/src/assets/components/BP_Log.jsx
+++ frontend/src/assets/components/BP_Log.jsx
@@ -15,52 +15,37 @@ const BP_Log = ({ responseData }) => {
   }
 
   return (
-    <div className="p-2 mb-1 rounded-lg bg-white">
+    <div className="p-2 mb-1 rounded-lg">
       {responseData.bp_log.date.map((date, index) => {
         const currentHigh = responseData.bp_log.high[index];
         const currentLow = responseData.bp_log.low[index];
 
-        if (index === 0) {
-          return (
-            <div key={index} className="flex flex-col mb-1">
-              <div className="flex items-center mb-2 bg-slate-100 rounded-md mx-1 p-2">
+        // Skip the log entry if high and low values are empty
+        if (!currentHigh && !currentLow) {
+          return null;
+        }
+
+        const isFirstValueOfDay =
+          index === 0 || responseData.bp_log.date[index - 1] !== date;
+
+        return (
+          <div key={index} className="flex flex-col mb-1">
+            {isFirstValueOfDay && (
+              <div className="flex items-center mb-2 bg-slate-200 rounded-md mx-1 p-2">
                 <div className="h-2 w-2 bg-gray-700 rounded-full mr-2"></div>
                 <h2 className="text-lg font-semibold text-gray-900">{date}</h2>
               </div>
+            )}
 
-              <div className="ml-1">
-                <div
-                  className={`text-sm text-gray-700 border border-gray-400 rounded-md p-3 flex justify-between items-center ${
-                    currentHigh > 190 && currentLow > 90
-                      ? "bg-purple-100"
-                      : currentHigh > 190
-                      ? "bg-red-100"
-                      : currentLow > 90
-                      ? "bg-orange-100"
-                      : ""
-                  }`}
-                >
-                  <div className="flex">
-                    <p className="font-semibold">{currentHigh}</p>
-                    <span className="text-gray-500 mx-1">/</span>
-                    <p>{currentLow}</p>
-                  </div>
-                  <div>High / Low</div>
-                </div>
-              </div>
-            </div>
-          );
-        } else if (responseData.bp_log.date[index - 1] === date) {
-          return (
-            <div key={index} className="ml-1 mb-1">
+            <div className="ml-1">
               <div
-                className={`text-sm text-black rounded-md border border-gray-400 p-3 flex items-center justify-between ${
+                className={`text-sm text-gray-700 border border-gray-400 rounded-md p-3 flex justify-between items-center ${
                   currentHigh > 190 && currentLow > 90
-                    ? "bg-red-100"
+                    ? "bg-purple-100"
                     : currentHigh > 190
-                    ? "bg-orange-100"
+                    ? "bg-red-100"
                     : currentLow > 90
-                    ? "bg-pink-100"
+                    ? "bg-orange-100"
                     : ""
                 }`}
               >
@@ -72,15 +57,8 @@ const BP_Log = ({ responseData }) => {
                 <div>High / Low</div>
               </div>
             </div>
-          );
-        } else {
-          return (
-            <div key={index} className="flex items-center mb-2">
-              <div className="h-2 w-2 bg-gray-700 rounded-full mr-2"></div>
-              <h2 className="text-xl font-semibold text-gray-800">{date}</h2>
-            </div>
-          );
-        }
+          </div>
+        );
       })}
     </div>
   );
