ae4884912e579a32e8b604f01a98073b3e15c7ba
Kavya Dua <kavyadua03@gmail.com>
2023-09-09 00:12:11 +0530

    setup admin panel

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import AppUser, PatientProfile, DoctorProfile, symptoms_diseases
+
+admin.site.register(AppUser)
+admin.site.register(PatientProfile)
+admin.site.register(DoctorProfile)
+admin.site.register(symptoms_diseases)
 
 # Register your models here.
--- Accounts/models.py
+++ Accounts/models.py
@@ -28,6 +28,7 @@ class AppUserManager(BaseUserManager):
         if not password:
             raise ValueError('A password is required.')
         user = self.create_user(email, password)
+        user.is_staff = True
         user.is_superuser = True
         user.save()
         return user
@@ -37,9 +38,11 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
     user_id = models.AutoField(primary_key=True)
     email = models.EmailField(max_length=50, unique=True)
     username = models.CharField(max_length=50)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
     objects = AppUserManager()
 
     def __str__(self):
